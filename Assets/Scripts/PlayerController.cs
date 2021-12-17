3de294772c002fa799c23399663ad33c5af3e8b6
pvicugna11 <pvicugna11@gmail.com>
2021-12-17 15:56:32 +0900

    Research and Troubleshooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
-    [SerializeField] private float turnSpeed = 25.0f;
+    // [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float horsePower = .0f;
+    [SerializeField] private int speed;
+    [SerializeField] private float rpm;
+    private const float turnSpeed = 25.0f;
     private float horizontalInput;
-    private float forwardInput;
+    private float verticalInput;
+    private Rigidbody playerRb;
+
+    [SerializeField] private GameObject centerOfMass;
+    [SerializeField] private TextMeshProUGUI speedmeterText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+
+    [SerializeField] private List<WheelCollider> allWheels;
+    [SerializeField] private int wheelsOnGround;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
 
     void FixedUpdate()
     {
-        // Input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        if (IsOnGround())
+        {
+            // Input
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
 
-        // Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+            // Moves the car forward based on vertical input
+            // transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+            playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+            // Rotates the car based on horizontal input
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f);
+            speedmeterText.SetText("Speed: " + speed + "kph");
+
+            rpm = (speed % 30) * 40;
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    private bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded) wheelsOnGround++;
+        }
+
+        return (wheelsOnGround == allWheels.Count);
     }
 }
